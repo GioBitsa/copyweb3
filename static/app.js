af00da1d64ccd8df9f792dc55b88148cb979011d
Giorgi <giorgi.bitsadze01@gmail.com>
2021-10-26 22:01:17 +0400

    .

--- static/app.js
+++ static/app.js
@@ -378,3 +378,99 @@ function close_overlay7() {
 // carousels
 
 // pricing carousel
+
+
+let isDown = false;
+let startX;
+let scrollLeft;
+const slider = document.querySelector('.slide-items');
+
+const end = () => {
+	isDown = false;
+  slider.classList.remove('active');
+}
+
+const start = (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;	
+}
+
+const move = (e) => {
+	if(!isDown) return;
+
+  e.preventDefault();
+  const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
+  const dist = (x - startX);
+  slider.scrollLeft = scrollLeft - dist;
+}
+
+(() => {
+	slider.addEventListener('mousedown', start);
+	slider.addEventListener('touchstart', start);
+
+	slider.addEventListener('mousemove', move);
+	slider.addEventListener('touchmove', move);
+
+	slider.addEventListener('mouseleave', end);
+	slider.addEventListener('mouseup', end);
+	slider.addEventListener('touchend', end);
+})();
+
+
+const featureBtnNext = document.getElementById('feature-btn-next');
+
+// featureBtnNext.addEventListener('click', (e) => {
+  
+// })
+
+
+
+// feature carousel
+
+
+let isDown2 = false;
+let startX2;
+let scrollLeft2;
+const slider2 = document.querySelector('.slide-items2');
+
+const end2 = () => {
+	isDown2 = false;
+  slider2.classList.remove('active');
+}
+
+const start2 = (e) => {
+  isDown2 = true;
+  slider2.classList.add('active');
+  startX2 = e.pageX || e.touches[0].pageX - slider2.offsetLeft;
+  scrollLeft2 = slider2.scrollLeft;	
+}
+
+const move2 = (e) => {
+	if(!isDown2) return;
+
+  e.preventDefault();
+  const x2 = e.pageX || e.touches[0].pageX - slider2.offsetLeft;
+  const dist2 = (x2 - startX2);
+  slider2.scrollLeft = scrollLeft2 - dist2;
+}
+
+(() => {
+	slider2.addEventListener('mousedown', start2);
+	slider2.addEventListener('touchstart', start2);
+
+	slider2.addEventListener('mousemove', move2);
+	slider2.addEventListener('touchmove', move2);
+
+	slider2.addEventListener('mouseleave', end2);
+	slider2.addEventListener('mouseup', end2);
+	slider2.addEventListener('touchend', end2);
+})();
+
+
+// const featureBtnNext = document.getElementById('feature-btn-next');
+
+// featureBtnNext.addEventListener('click', (e) => {
+  
+// })
